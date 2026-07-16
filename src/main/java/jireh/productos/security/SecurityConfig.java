944eb3a3dd899ccdcfa5d5759ece3f5ce7a5275b
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2026-07-16 11:44:17 -0300

    Update SecurityConfig.java

--- src/main/java/jireh/productos/security/SecurityConfig.java
+++ src/main/java/jireh/productos/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
