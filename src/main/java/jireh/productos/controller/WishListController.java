1407dd51c0fc0a80c5ea5564c61b2e9e247e56f0
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2026-08-01 01:28:09 -0300

    Update WishListController.java

--- src/main/java/jireh/productos/controller/WishListController.java
+++ src/main/java/jireh/productos/controller/WishListController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "products/wishlist")
+@RequestMapping("/products/wishlist")
 public class WishListController {
 
     @Autowired
