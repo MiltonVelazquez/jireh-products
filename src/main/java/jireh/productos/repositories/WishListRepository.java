8bf85fbb7fb8273ac4b7fcde0947491bedb3268e
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2026-08-01 15:58:10 -0300

    Update WishListRepository.java

--- src/main/java/jireh/productos/repositories/WishListRepository.java
+++ src/main/java/jireh/productos/repositories/WishListRepository.java
@@ -1,17 +1,16 @@
 package jireh.productos.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import jireh.productos.models.WishListEntity;
 
-public interface WishListRepository extends CrudRepository<WishListEntity, Long>{
+public interface WishListRepository extends CrudRepository<WishListEntity, Long> {
 
     List<WishListEntity> findByUserId(Long userId);
 
-    Optional<WishListEntity> findByUserIdAndProductId(Long userId, Long productId);
+    List<WishListEntity> findByUserIdAndProductId(Long userId, Long productId);
 
     boolean existsByUserIdAndProductId(Long userId, Long productId);
 }
