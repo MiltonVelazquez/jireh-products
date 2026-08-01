12a5b62a5d7dd7b739459c85857e382e23757024
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2026-07-31 23:34:18 -0300

    Update SecurityConfig.java

--- src/main/java/jireh/productos/security/SecurityConfig.java
+++ src/main/java/jireh/productos/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,22 +31,15 @@ public class SecurityConfig {
         http
             .cors(withDefaults()) 
             .csrf(config -> config.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                // .requestMatchers(HttpMethod.GET, "/products/product/**").permitAll()
-                // .requestMatchers(HttpMethod.GET, "/products/category/**").permitAll()
-                // .requestMatchers(HttpMethod.GET, "/products/subcategory/**").permitAll()
-                // .requestMatchers(HttpMethod.GET, "/products/calification/product/**").permitAll()
-
-                // .requestMatchers(HttpMethod.POST, "/products/product/**").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.DELETE, "/products/product/**").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.POST, "/products/category/**").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.POST, "/products/subcategory/**").hasRole("ADMIN")
-
-                // .requestMatchers("/products/wishlist/**").hasRole("USER")
-                // .requestMatchers(HttpMethod.POST, "/products/calification/**").hasAnyRole("USER", "ADMIN")
-
-                // .anyRequest().authenticated()
-                .anyRequest().permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/**", "/category/**", "/subcategory/**", "/product/**").permitAll()
+                
+                .requestMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
+                
+                .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
@@ -52,13 +47,14 @@ public class SecurityConfig {
 
         return http.build();
     }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
         configuration.setAllowedOrigins(Arrays.asList(
             "https://eddc0f65-a17f-4af9-9774-b722606f3449-00-2zbw4m3gak2ba.worf.replit.dev",
-            "https://jireh-frontend-chi.vercel.app/"
+            "https://jireh-frontend-chi.vercel.app"
         ));
         
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
@@ -70,12 +66,11 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        
         grantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); 
-        
         grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
@@ -86,7 +81,6 @@ public class SecurityConfig {
     @Bean
     public JwtDecoder jwtDecoder() {
         String secretKey = "dad7150098c718a48c716173593961ca661da5efc48c158f1d1e46b115afe845"; 
-    
         SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
         return NimbusJwtDecoder.withSecretKey(secretKeySpec).build();
     }
