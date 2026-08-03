bda9f63b5f757eaf222038c85fa2069ea9a1e895
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2026-08-03 13:22:15 -0300

    Update CalificationController.java

--- src/main/java/jireh/productos/controller/CalificationController.java
+++ src/main/java/jireh/productos/controller/CalificationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jireh.productos.dto.ReviewDTO;
 import jireh.productos.models.CalificationEntity;
